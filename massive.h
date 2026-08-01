06d9f15b8b062b138d34c37aed60584fad4f51c5
PisunBobraHacker <gorohnl66@gmail.com>
2026-08-01 17:37:38 +0700

    Delete massive.h

--- massive.h
+++
@@ -1,9 +0,0 @@
-// massive.h
-#ifndef MASSIVE_H
-#define MASSIVE_H
-
-extern const unsigned char mbr_gg[512];
-extern const unsigned char uefi_loader[];
-extern const unsigned int uefi_size;
-
-#endif // MASSIVE_H
